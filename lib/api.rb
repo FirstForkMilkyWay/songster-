ca7a391a4d920ed529836845d37a84d3c1b66c96
Stuart Fleishman <stuart.a.fleishman@gmail.com>
2020-10-18 18:55:46 +0000

    json

--- lib/api.rb
+++ lib/api.rb
@@ -1,10 +1,13 @@
 require 'pry'
+require 'rest-client'
+require 'json'
 
 class API 
 
   def self.fetch_songs 
-    url = "https://ghibliapi.herokuapp.com/films"
-    binding.pry 
+   url = "https://www.songsterr.com/a/ra/songs/byartists.json?artists=Metallica"
+   response = JSON.parse(RestClient.get(url).to_s)
+   binding.pry 
   end 
 
 
