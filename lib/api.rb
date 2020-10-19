3f15ff9dd3ad06a28837faa0a94bacc003f6a3dd
Stuart Fleishman <stuart.a.fleishman@gmail.com>
2020-10-19 22:00:05 +0000

    cleaned some code up

--- lib/api.rb
+++ lib/api.rb
@@ -5,26 +5,21 @@ require_relative "artist.rb"
 
 class API
 
-    attr_accessor :name, :artist, :tab_types
+
 
   def self.fetch_songs
    url = "https://www.songsterr.com/a/ra/songs/byartists.json?artists=Metallica"
    response = JSON.parse(RestClient.get(url).to_s)
-  #  array_of_songs = response[0]
-  #  array_of_songs.each do |song_name["title"]|
-    # response.each do |song_name|
        song = Songs.new
        song.name = response[0]["title"]
        song.artist = response[0]["artist"]["name"]
        song.tab_types = response[0]["tabTypes"]
 
      binding.pry
-   end
   end
 
-  def self.sort_song
 
-  end
+end
 
 
 
--- lib/songs.rb
+++ lib/songs.rb
@@ -2,31 +2,7 @@ class Songs
 
     attr_accessor :name, :artist, :tab_types
 
-    def initialize
-      @name = name
-      @artist = artist
-      @tab_types = tab_types
-    end
 
-    def name=(name)
-      @name = name
-    end
-
-    def artist=(artist)
-      @artist = artist
-    end
-
-    def artist
-      @artist
-    end
-
-    def tab_types=(tab_types)
-      @tab_types = tab_types
-    end
-
-    def tab_type
-      @tab_type
-    end 
 
 
 
